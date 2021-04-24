91c34024f07f52d1aae9a3e8c42bca4ed458ebbf
Lenka Verzichová <lenkaverzichova@gmail.com>
2021-04-24 11:50:47 +0200

    Zavírání navigace po kliku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navigaceElm = document.querySelector('#nav-btn');
+navigaceElm.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+const navigaceOdkazy = document.querySelectorAll('a');
+navigaceOdkazy.forEach((polozkaNavigace) => {
+  //console.log(polozkaNavigace);
+  polozkaNavigace.addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('nav-closed');
+  });
+});
